ad8136fde85d8ba584afe1e0e5eb66083bb19baa
muthamizhyadav <muthamizhyadav@gmail.com>
2023-03-24 14:31:12 +0530

    adsf

--- src/components/State/EditeState.js
+++ src/components/State/EditeState.js
@@ -87,10 +87,29 @@ function EditeState() {
             ? stateData.img.map((e) => (
                 <>
                   <img src={e} alt="image" />
-                  <button onClick={() => imageDel(e)}>delete</button>
+                  <button onClick={() => imageDel(e)}>
+                    <svg
+                      width="18"
+                      height="16"
+                      viewBox="0 0 18 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M15.7692 4.18182L15.2564 13.2029C15.2288 13.6891 15.0225 14.146 14.6797 14.4801C14.337 14.8142 13.8837 15.0002 13.4127 15H4.58728C4.11628 15.0002 3.66301 14.8142 3.32027 14.4801C2.97752 14.146 2.77122 13.6891 2.74359 13.2029L2.23077 4.18182M7.15385 7.68182L9 9.59091M9 9.59091L10.8462 11.5M9 9.59091L10.8462 7.68182M9 9.59091L7.15385 11.5M1.92308 4.18182H16.0769C16.5865 4.18182 17 3.75418 17 3.22727V1.95455C17 1.42764 16.5865 1 16.0769 1H1.92308C1.41354 1 1 1.42764 1 1.95455V3.22727C1 3.75418 1.41354 4.18182 1.92308 4.18182Z"
+                        stroke="#FF0000"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </button>
                 </>
               ))
             : ""}
+        </div>
+        <div className="edite-del-btn">
+          <button>Save & Continue</button>
         </div>
       </div>
     </div>
@@ -98,4 +117,3 @@ function EditeState() {
 }
 
 export default EditeState;
-
